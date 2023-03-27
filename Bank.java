dd5e4a2a115e42d82763aafc67f83c93756246f9
shanmugapriya-gs <shanmugapriya95ece@gmail.com>
2023-03-27 16:24:26 +0530

    New changes

--- Bank.java
+++ Bank.java
@@ -3,6 +3,7 @@ package com.day11.oops2;
 public class Bank extends ReserveBank{
 	String branch;
 	public String getLoanIntrestAmount(int principalAmount) {
+		System.out.println("");
 		return "Intrest Amount is " + (principalAmount*10/100);
 }
 }
